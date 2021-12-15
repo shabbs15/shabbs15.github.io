52ac4dc22e8d5e7767a11d524bb7ee698f83ff47
Shabbs15 <Sm5648792@gmail.com>
2021-12-15 22:01:51 +0000

    following a flask tutorial

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Corey Schafer",
+        "title": "blog post 1",
+        "content": "first post content",
+        "date_posted": "April 20, 2018"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "ecomerce",
+        "content": "check out this ecommers",
+        "date_posted": "January 24, 2014"
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hell, world"
-    return render_template("index.html")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="about")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
