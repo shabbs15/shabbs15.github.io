68b08d443f3a7d3fa3230eeb50ccca1af8388999
Shabbs15 <Sm5648792@gmail.com>
2021-12-16 13:13:01 +0000

    post, and other things, sessions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "hello"
 
 posts = [
     {
@@ -26,6 +27,30 @@ def home():
 def about():
     return render_template("about.html", title="about")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    print("loginging in")
+    if request.method=="POST":
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user", usr=user))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html", title="login")
+
+@app.route("/user")
+def user():
+    if  "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
