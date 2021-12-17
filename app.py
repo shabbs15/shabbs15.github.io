b07b571e74dc3d843176e1310f71ba4550a06c0c
Shabbs15 <Sm5648792@gmail.com>
2021-12-17 23:30:18 +0000

    i think if xide the bug

--- app.py
+++ app.py
@@ -50,13 +50,14 @@ def user():
 
 @app.route("/ip")
 def ip():
+    print(request.remote_addr)
     ip = request.headers.getlist("X-Forwarded-For")[0]
     print("asdf",ip)
     ip = ip.split(":")[0]
     print("adfasd",ip)
 
     r = req.get("http://ipinfo.io/"+ ip).json() # "151.101.193.69"
-    if not r["bogon"]:
+    if "bogon" in r:
         return r["city"] +", " + r["country"] + " this you? <br><br> Sorry this is saved mon ami, better chance next time"
     else:
         return "yo? odd" + request.remote_addr + ip
