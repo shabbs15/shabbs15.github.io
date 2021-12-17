ffdcf922a26dad8d25808692273729f87e7a6404
Shabbs15 <Sm5648792@gmail.com>
2021-12-17 22:25:57 +0000

    .

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ def user():
 
 @app.route("/ip")
 def ip():
-    r = req.get("http://ipinfo.io/"+ request.environ.get('HTTP_X_REAL_IP', request.remote_addr)   ).json() # "151.101.193.69"
+    r = req.get("http://ipinfo.io/"+ request.headers.getlist("X-Forwarded-For")[0]   ).json() # "151.101.193.69"
     if not r["bogon"]:
         return r["city"] +", " + r["country"] + " this you? <br><br> Sorry this is saved mon ami, better chance next time"
     else:
-        return "yo? odd" + request.remote_addr + request.environ.get('HTTP_X_REAL_IP', request.remote_addr)   
+        return "yo? odd" + request.remote_addr + request.headers.getlist("X-Forwarded-For")[0]   
 
 @app.route("/logout")
 def logout():
